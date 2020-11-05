038e54385de1d24acfb1900733d16e5710739268
FraidoonOmarzai <fraidoon_omarzai@yahoo.com>
2020-11-05 08:44:30 +0530

    (Authentication) level 1 - Username and Password

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+});
+
+const userSchema = {
+   email: String,
+   password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+   res.render("home");
+});
+
+app.get("/login", (req, res) => {
+   res.render("login");
+});
+
+app.get("/register", (req, res) => {
+   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+   const userReg = new User({
+      email: req.body.username,
+      password: req.body.password
+   });
+
+   userReg.save(err => {
+      if (!err) {
+         res.render("secrets");
+      } else {
+         console.log(err);
+      }
+   })
+});
+
+app.post("/login", (req, res) => {
+   const username = req.body.username;
+   const password = req.body.password;
+
+   User.findOne({
+         email: username
+      },
+      (err, found) => {
+         if (!err) {
+            if (found) {
+               if (found.password === password) {
+                  res.render("secrets");
+               }
+            }
+         } else {
+            console.log(err);
+         }
+      }
+   );
+});
+
+
+
+
+
+
+
+app.listen(3000, () => {
+   console.log("running on port 3000");
+})
